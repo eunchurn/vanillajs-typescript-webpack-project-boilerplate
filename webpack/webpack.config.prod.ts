62bb7f3ac3c92f7a1e17693a7e3bc649543d2178
Eunchurn Park <eunchurn.park@gmail.com>
2022-02-26 17:15:53 +0900

    feat(imagemin): image optimizer

--- webpack/webpack.config.prod.ts
+++ webpack/webpack.config.prod.ts
@@ -3,6 +3,8 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
 import TerserPlugin from "terser-webpack-plugin";
+import ImageMinimizerPlugin from "image-minimizer-webpack-plugin";
+
 import { config, baseDirectory } from "./webpack.config.common";
 
 const prod: Configuration = {
@@ -17,6 +19,29 @@ const prod: Configuration = {
         },
       }),
       new TerserPlugin({ terserOptions: { format: { comments: false } }, extractComments: false }),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              [
+                "svgo",
+                {
+                  plugins: [
+                    {
+                      name: "preset-default",
+                      params: { overrides: { inlineStyles: { onlyMatchedOnce: false } }, removeDoctype: false },
+                    },
+                  ],
+                },
+              ],
+            ],
+          },
+        },
+      }),
     ],
   },
   plugins: [
